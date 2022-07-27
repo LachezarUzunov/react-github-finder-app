c10df1421aff9d8c04113888638fea041a874cce
lachezaruzunov <uzunov_89_lacho@abv.bg>
2022-07-27 08:57:28 +0300

    Adding initial routes and notFound page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import NotFound from "./Pages/NotFound";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
